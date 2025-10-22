e59f43e91b93a66188a9116e089cce682f459a84
divakaran.s <divakaran.s@continube.com>
2025-10-22 15:53:08 +0530

    changes

--- constants/constants.py
+++ constants/constants.py
@@ -14,9 +14,7 @@ else:
     basic_auth_flow = True
     headers = {"Authorization": "Basic " + base64.b64encode((cid + ":" + cs).encode("ascii")).decode("ascii")}
 
-host = os.environ.get("CCOW_HOST", "https://dev.compliancecow.live")
-if not host.endswith("/api"):
-    host += "/api"
+host = os.environ.get("CCOW_HOST", "http://cowapiservice:9080")
 
 ENABLE_CONTEXTUAL_VECTOR_SEARCH = os.environ.get("ENABLE_CONTEXTUAL_VECTOR_SEARCH", "false").lower() == "true"
 
